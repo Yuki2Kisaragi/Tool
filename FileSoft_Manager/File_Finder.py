a1225c4bc52330cb2be56da5e41d2036b854ede2
Yuki2Kisaragi <50705610+Yuki2Kisaragi@users.noreply.github.com>
2019-10-22 19:30:35 +0900

    Update File_Finder.py

--- FileSoft_Manager/File_Finder.py
+++ FileSoft_Manager/File_Finder.py
@@ -8,35 +8,32 @@ Search_path = r"SEARCH_PATH"
 folder = []
 
 
-# TO DO .c,.hファイルを探してリスト型データにする
+# .c/.hファイルを探してリスト型データにする
 def file_finder(_path):
-    os.chdir(_path)
     temp = Path(_path)
     file_list = [p for p in temp.glob("**/*") if re.search(".(c|h)", str(p))]
-
+    # .c /.h以外のファイルも取り込んでいる。要改善
     for path in file_list:
         if not os.path.isdir(path):
-            folder.append(os.path.relpath(path, Search_path))
+            folder.append(path)
+
     return folder
 
 
-# TO DO  リストから全ファイルを開き、1行目だけを抜き出して閉じる。辞書型にして{相対path:"1行目"}となるようにする
+# リストから全ファイルを開き、1行目だけを抜き出して閉じる。辞書型にして{相対path:"1行目"}となるようにする
 def file_read(*folder_list):
     read_data = {}
     for j in folder_list:
-
         if os.path.isdir(j):
-            with open(j, "r") as f:
-                read_data[j] = f.readline()
-            f.close()
+            os.chdir(j)
         else:
-            with open(j, "r") as f:
-                read_data[j] = f.readline()
+            with open(j, "r", encoding="utf8") as f:
+                read_data[str(j)] = f.readline()
             f.close()
     return read_data
 
 
-# TO DO  一つのテキストファイルとして出力する
+# 一つのテキストファイルとして出力する
 def file_list_generator(**dictionary):
     os.chdir(Output_path)
     with open("soft_list.txt", "w") as f:
@@ -44,9 +41,14 @@ def file_list_generator(**dictionary):
             temp = str(i).replace("/* ", "")
             temp = temp.replace(" */", "")
             f.write("{}  : {}\n".format(k, temp))
+
     f.close()
 
 
-file_list = file_finder(Search_path)
-file_dict = file_read(*file_list)
-file_list_generator(**file_dict)
+def main():
+    file_list = file_finder(Search_path)
+    file_dict = file_read(*file_list)
+    file_list_generator(**file_dict)
+
+
+main()
